27c9f2d96f1640f5d668adaeb78b43e79521abf4
Osef1712 <j.zumkir@ludus-academie.com>
2019-12-09 11:12:32 +0100

    Premier commit

---
+++ E1.pas
@@ -0,0 +1,27 @@
+//ALGORITHME: Algo_1
+//BUT	:Doubler un nombre
+//ENTREE:Utilisateur donne un nombre
+//SORTIE:Double le nombre
+
+//Var val, double :ENTIER
+
+//DEBUT
+	//Val ← 231
+	//Double ← Val * 2
+	//ECRIRE Val
+	//ECRIRE Double
+//FIN
+program E1;
+
+uses crt;
+
+var val,double: integer;
+
+Begin;
+	clrscr;
+	Val := 231;
+	Double := Val*2;
+	Writeln (Val);
+	Writeln (Double);
+	Readln;
+End.
